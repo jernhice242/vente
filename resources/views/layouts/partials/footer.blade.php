d6a17eb8bc035dee99d0635f1c72e9d47728c388
valmaure <jeanvalere1948@gmail.com>
2024-06-13 13:31:49 +0100

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <!-- <b>{{ __('common.Version') }}</b> 3.0.3 -->
+    </div>
+    <strong>Copyright &copy; 2024 <a href="#">Jernhice le developpeur</a>.</strong> {{ __('common.all_reserved') }}
+  </footer>
